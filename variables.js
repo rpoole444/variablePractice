0731a9ac9eeed779e76bb96a16acef01e49a717c
Reid Poole <poole.reid@gmail.com>
2022-09-20 15:19:42 -0500

    finalize assignment

--- variables.js
+++ variables.js
@@ -1,6 +1,13 @@
-# **FUN**
-
+// **FUN**
 var name = "Reid Poole"
 var email = "poole.reid@gmail.com"
 var startingAge = 36
 var tutionPaid = false
+var name = "john doe"
+var email = "youknowit@gmail.com"
+var startingAge = 10
+var tuitionPaid = true
+console.log(name)
+console.log(email)
+console.log(startingAge)
+console.log(tuitionPaid)
